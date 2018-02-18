5cd6c9ef228d270b94e25bb0ea2df4967203e2ac
Dasha <daria1994yegorova@e-mail.ua>
2018-02-18 17:42:54 +0200

    Fixed

--- lesson6/Factorial.java
+++ lesson6/Factorial.java
@@ -2,15 +2,13 @@ package lesson6;
 
 public class Factorial {
     public static void main(String[] args) {
-        int n = getRandomNumber(100);
+        int n = getRandomNumber(30);
         System.out.println(n);
         int j = 1;
         for (int i = 1; i <= n; i++) {
             j *= i;
-            if (i == n) {
-                System.out.println(j);
-            }
         }
+        System.out.println(j);
     }
 
     static int getRandomNumber(int n) {
