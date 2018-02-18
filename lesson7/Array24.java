ee86cc793988f95f04843c8a7ac813d571a72a07
Dasha <daria1994yegorova@e-mail.ua>
2018-02-18 17:54:38 +0200

    Classworks

---
+++ lesson7/Array24.java
@@ -0,0 +1,18 @@
+package lesson7;
+
+import java.util.Arrays;
+
+/**
+ * Created by Student on 09.02.2018.
+ */
+public class Array24 {
+    public static void main(String[] args) {
+        char[][] array = {
+                {'a', 'b'},
+                {'c', 'd'},
+                {'e', 'f'},
+                {'j', 'h'}
+        };
+        System.out.println(Arrays.deepToString(array));
+    }
+}
