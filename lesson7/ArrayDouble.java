6387056b2e906e4fcd30f02144661af598014c42
Dasha <daria1994yegorova@e-mail.ua>
2018-02-18 17:48:46 +0200

    Classworks

---
+++ lesson7/ArrayDouble.java
@@ -0,0 +1,11 @@
+package lesson7;
+
+/**
+ * Created by Student on 09.02.2018.
+ */
+public class ArrayDouble {
+    public static void main(String[] args) {
+        Double[] value = {4.5, 6.7, 34.8, 234.7};
+        System.out.println(value[3]);
+    }
+}
