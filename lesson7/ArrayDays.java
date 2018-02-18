c8651a3ef4dbf6127ed4b763f7af6b2b1bad4e3f
Dasha <daria1994yegorova@e-mail.ua>
2018-02-18 17:51:24 +0200

    Classworks

---
+++ lesson7/ArrayDays.java
@@ -0,0 +1,18 @@
+package lesson7;
+
+/**
+ * Created by Student on 09.02.2018.
+ */
+public class ArrayDays {
+    public static void main(String[] args) {
+        String[] days = new String[7];
+        days[0] = "Monday";
+        days[1] = "Tuesday";
+        days[2] = "Wensday";
+        days[3] = "Thursday";
+        days[4] = "Friday";
+        days[5] = "Saturday";
+        days[6] = "Sunday";
+        System.out.println(days[6]);
+    }
+}
