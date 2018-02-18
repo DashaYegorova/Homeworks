e4bd11a44dee7e60885dba72ea83be1a67158857
Dasha <daria1994yegorova@e-mail.ua>
2018-02-18 18:21:09 +0200

    HW lesson9

--- lesson9/PersonDemo.java
+++ lesson9/PersonDemo.java
@@ -6,5 +6,10 @@ public class PersonDemo {
         p1.fullName = "Kevin";
         p1.age = 23;
         Person p2 = new Person("Nick", 25);
+
+        p1.move(p1);
+        p1.talk(p1);
+        p2.move(p2);
+        p2.talk(p2);
     }
 }
