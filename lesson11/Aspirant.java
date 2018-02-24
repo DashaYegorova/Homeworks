f1c247eb747ff43567234ab6f521878db9d6fa97
Dasha <daria1994yegorova@e-mail.ua>
2018-02-24 22:49:13 +0200

    Classwork

---
+++ lesson11/Aspirant.java
@@ -0,0 +1,19 @@
+package lesson11;
+
+public class Aspirant extends Student {
+
+    private String scienceWork;
+
+    public Aspirant(String firstName, String lastName, String group, double averageMark, String scienceWork) {
+        super(firstName, lastName, group, averageMark);
+        this.scienceWork = scienceWork;
+    }
+
+    public String getScienceWork() {
+        return scienceWork;
+    }
+
+    public void setScienceWork(String scienceWork) {
+        this.scienceWork = scienceWork;
+    }
+}
---
+++ lesson11/StudentDemo.java
@@ -0,0 +1,15 @@
+package lesson11;
+
+public class StudentDemo {
+    public static void main(String[] args) {
+        Student student1 = new Aspirant("Daria", "Yegorova", "OF-81",
+                4.6, "Madness as philosophical problem");
+        Aspirant aspirant1 = new Aspirant("Daria", "Yegorova", "OF-81",
+                4.6, "Madness as philosophical problem");
+        Student student2 = new Student("Daria", "Yegorova", "OF-81",
+                4.6);
+        System.out.println(student1.getFirstName() + student1.getLastName() + " " + student1.getAverageMark()
+                + " " + student1.getGroup() + " " + student2.getFirstName() + " " + aspirant1.getFirstName()
+                + " " + aspirant1.getScienceWork());
+    }
+}
