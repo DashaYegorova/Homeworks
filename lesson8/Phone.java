7a46b9d3e4b9c1f22d4d49f4bc60880ba3d44ed3
Dasha <daria1994yegorova@e-mail.ua>
2018-02-24 22:51:19 +0200

    Classwork

--- lesson8/Phone.java
+++ lesson8/Phone.java
@@ -4,7 +4,66 @@ package lesson8;
  * Created by Student on 12.02.2018.
  */
 public class Phone {
-    String number;
-    String model;
-    double weight;
+    private static int count;
+    private String number = "0655345676";
+    private String model = "lenovo";
+    private double weight = 4.5;
+
+    Phone(String number, String model, double weight) {
+        this(number, model);
+        this.weight = weight;
+    }
+
+    Phone(String n, String m) {
+        number = n;
+        model = m;
+        count++;
+    }
+
+    Phone() {
+        count++;
+    }
+
+    public static int getCount() {
+        return count;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    void recieveCall(String name) {
+        System.out.println("Звонит " + name);
+    }
+
+    void recieveCall(String name, String number) {
+        System.out.println("Звонит " + name + " с номером телефона " + number);
+    }
+
+    void sendMessage(String... numbers) {
+        System.out.print("Сообщение отправлено ");
+        for (String number : numbers) {
+            System.out.print(number + ", ");
+        }
+    }
 }
--- lesson8/PhoneDemo.java
+++ lesson8/PhoneDemo.java
@@ -6,24 +6,23 @@ package lesson8;
 public class PhoneDemo {
     public static void main(String[] args) {
 
-        Phone phone1 = new Phone();
-        Phone phone2 = new Phone();
+        Phone phone1 = new Phone("067984635", "Lenovo", 2.3);
+        Phone phone2 = new Phone("067984635", "Apple");
         Phone phone3 = new Phone();
+        System.out.println(Phone.getCount());
 
-        phone1.number = "067984635";
-        phone1.model = "Lenovo";
-        phone1.weight = 2.3;
 
-        phone2.number = "067984635";
-        phone2.model = "Apple";
-        phone2.weight = 4.5;
+        phone3.setNumber("067984635");
+        phone3.setModel("Nokia");
+        phone3.setWeight(6.7);
 
-        phone3.number = "067984635";
-        phone3.model = "Nokia";
-        phone3.weight = 6.7;
-
-        System.out.println("Номер: " + phone1.number + " Модель: " + phone1.model + " Вес: " + phone1.weight);
-        System.out.println("Номер: " + phone2.number + " Модель: " + phone2.model + " Вес: " + phone2.weight);
-        System.out.println("Номер: " + phone3.number + " Модель: " + phone3.model + " Вес: " + phone3.weight);
+        System.out.println("Номер: " + phone1.getNumber() + " Модель: " + phone1.getModel() + " Вес: " + phone1.getWeight());
+        System.out.println("Номер: " + phone2.getNumber() + " Модель: " + phone2.getModel() + " Вес: " + phone2.getWeight());
+        System.out.println("Номер: " + phone3.getNumber() + " Модель: " + phone3.getModel() + " Вес: " + phone3.getWeight());
+        phone1.recieveCall("Mom");
+        System.out.println(phone1.getNumber());
+        phone1.recieveCall("Dad", phone2.getNumber());
+        phone1.sendMessage(phone2.getNumber(), phone3.getNumber(), "57783456876");
+        phone2.sendMessage("5667645665", "5467655");
     }
 }
