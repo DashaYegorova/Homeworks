c9e7e6a86fc914f163f37a0fe8fc1cba8f353e01
Dasha <daria1994yegorova@e-mail.ua>
2018-03-01 00:34:09 +0200

    Дз по Animals

---
+++ lesson12/Animals/Animal.java
@@ -0,0 +1,39 @@
+package lesson12.Animals;
+
+public class Animal {
+    private String food;
+    private String location;
+
+    public Animal(String food, String location) {
+        this.food = food;
+        this.location = location;
+    }
+
+    public String getFood() {
+        return food;
+    }
+
+    public void setFood(String food) {
+        this.food = food;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public void makeNoize() {
+        System.out.println("Животное издаёт звуки");
+    }
+
+    public void eat() {
+        System.out.println("Животное ест");
+    }
+
+    public void sleep() {
+        System.out.println("Животное спит");
+    }
+}
